5b00d8fa644e245c35be3c7233449e4c702cc44e
SATYAM <55735057+coderjoker666@users.noreply.github.com>
2020-10-18 20:45:29 +0530

    reversing_number.c

--- ReverseNumber.c
+++ ReverseNumber.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-int main() {
-    int n, rev = 0, remainder;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-    while (n != 0) {
-        remainder = n % 10;
-        rev = rev * 10 + remainder;
-        n /= 10;
-    }
-    printf("Reversed number = %d", rev);
-    return 0;
+int ReversingNumber(int Num)       /*function to reverse the number*/
+{
+int Reverse_Number = 0;
+    
+while(Num > 0)                  
+{
+Reverse_Number = Reverse_Number*10 + Num%10;
+    
+Num = Num/10;
+}
+return Reverse_Number;                    /*Returning our reversed number*/
+}
+int main()
+{
+int Num;
+printf("Enter the number to be reversed:n");
+scanf("%d", &Num);
+printf("Reverse of no. is %d", ReversingNumber(Num)); 
+getchar();
+return 0;
 }
